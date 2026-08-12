c28fc64227b504b46401fa3a7662ded5eef08b0f
Pardhiv <pardhivreddy22@gmail.com>
2026-08-12 22:39:14 +0530

    initial backend

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ class Settings:
     def __init__(self):
         self.host = os.getenv("DB_HOST")
         self.user = os.getenv("DB_USER")
-        self.port = os.getenv("DB_PORT")
+        self.port = int(os.getenv("DB_PORT"))
         self.password = os.getenv("DB_PASS")
         self.name = os.getenv("DB_NAME")
         self.skey = os.getenv("SECRET_KEY")
